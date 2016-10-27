ec9be321b8032ec350e7d53f5b6dc8f67449000c
Michael McCoy <mmccoy@google.com>
2016-10-27 12:54:19 -0700

    Bug fixes Mozilla

--- client/components/navbar/navbar.component.js
+++ client/components/navbar/navbar.component.js
@@ -12,7 +12,7 @@ export class NavbarComponent {
       // },
       {
         title: 'Custom Sticks',
-        state: 'customize'
+        state: 'customize.player'
       }
     ],
     // links: [
